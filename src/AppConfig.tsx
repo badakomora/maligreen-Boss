63effcac3ac8d16caf859f61ae2dc8e1f22ccbac
badaandrew <badakomora06@gmail.com>
2025-05-22 21:11:19 +0300

    server end point

--- src/AppConfig.tsx
+++ src/AppConfig.tsx
@@ -1,2 +1,4 @@
 // local
-export const serverUrl = "http://localhost:4000/";
+// export const serverUrl = "http://localhost:4000/";
+export const serverUrl = "https://maligreen-server.onrender.com/";
+
